0fbb5e642bb8d3700c963c0836319df3258d4004
Jess Y <31145424+jcat1504@users.noreply.github.com>
2021-06-24 22:18:49 -0700

    Update LiveWeather.sh

--- weatherScript/LiveWeather.sh
+++ weatherScript/LiveWeather.sh
@@ -120,3 +120,19 @@ then
 break
 fi
 done
+
+```
+const twilio = require('twilio');
+require('dotenv').config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID; 
+const authToken = process.env.TWILIO_AUTH_TOKEN;  
+
+const client = new twilio(accountSid, authToken);
+
+client.messages.create({
+    body: callWeather,
+    to: '+19787264295', 
+    from: '+14159431419' 
+})
+.then((message) => console.log(message.sid));

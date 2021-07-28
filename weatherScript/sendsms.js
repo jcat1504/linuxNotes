8423189f6cc3a47359490b2ab284a9e6353b2f91
Jess Y <31145424+jcat1504@users.noreply.github.com>
2021-07-27 18:04:45 -0700

    Update sendsms.js

--- weatherScript/sendsms.js
+++ weatherScript/sendsms.js
@@ -41,3 +41,33 @@ client.messages.create({
    from: '+14159431419'
  })
 //.then((message) => console.log(message.sid));
+
+//RESPOND TO AN INCOMING TEXT MESSAGE
+//how my thought process would be is... 
+//1. to make sure this simple code works. so I will just send a simple text message from 
+//current phone number to twilio number
+//2. request current weather only in current location
+//3. request current weather in current location OR anywhere else in the world 
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello!');
+});
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+
+  twiml.message('Hello there!');
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(3000, () => {
+  console.log('Express server listening on port 3000');
+});
+
